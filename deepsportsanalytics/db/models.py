ef9512593d68d89109f7ab896da61992df9aee46
Andrej <supius@gmail.com>
2015-10-25 17:34:39 +0300

    add prediction service

--- deepsportsanalytics/db/models.py
+++ deepsportsanalytics/db/models.py
@@ -19,3 +19,17 @@ class UpdateStatModel(Base):
         # 0 - initial, 1 - in-progress, 2 - finished
         self.status = status
         self.start_date = datetime.utcnow()
+
+class StatModel(Base):
+    __tablename__ = 'stat_model'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    type = Column(String(50), nullable=False)
+    create_date = Column(DateTime, default=datetime.utcnow())
+    update_date = Column(DateTime, default=datetime.utcnow())
+
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+        self.create_date = datetime.utcnow()
+        self.update_date = datetime.utcnow()
--- deepsportsanalytics/services/prediction_service.py
+++ deepsportsanalytics/services/prediction_service.py
@@ -1,3 +1,5 @@
+from db.models import StatModel
+
 class PredictionService(object):
 
     def __init__(self, model_storage):
@@ -7,6 +9,9 @@ class PredictionService(object):
         return self.__model_storage.get(model_name).predict(data)
 
     def update(self, data, model_name):
-        model = self.__model_storage.get(model_name)
-        model.update(data)
+        # model = self.__model_storage.get(model_name)
+        # model.update(data)
+
+        model_type = StatModel.query.filter(StatModel.name == model_name=).first().type
+
         model_storage.set(model_name)

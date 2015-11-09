8ad9cc83c6ce9fb50a64fe92a210315aa09e0c22
Andrej <supius@gmail.com>
2015-11-09 22:47:34 +0300

    add mongo src

---
+++ deepsportsanalytics/data/source/mongodb_source.py
@@ -0,0 +1,22 @@
+import sys
+import datetime
+from dateutil.parser import parse
+import logging
+
+from pymongo import MongoClient
+
+from source_base import DataSourceBase
+from shared.cache import CacheBase
+
+logger = logging.getLogger(__name__)
+
+class MongoDBDataSource(DataSourceBase):
+
+    def __init__(self, url, model_name, db_name):
+        self.__db = MongoClient(url)[db_name]
+        self.__model_name = model_name
+
+    def load(self, filter):
+        logger.info('load')
+        for row in self.__db['%s_data' % self.__model_name].find(filter):
+            yield return row

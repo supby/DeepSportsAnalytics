34a4e8e9890de0a06dc213c3782df39e24e5e5d2
Andrej <supius@gmail.com>
2015-11-10 00:08:04 +0300

    ref config

--- deepsportsanalytics/webapp/app.py
+++ deepsportsanalytics/webapp/app.py
@@ -1,10 +1,3 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-__author__ = "andrej"
-__date__ = "$Jun 8, 2015 9:49:09 PM$"
-
 import os
 import sys
 import logging
@@ -16,48 +9,24 @@ from flask import Flask, request, redirect, url_for
 from flask import render_template
 
 import global_config
-from shared.cache import DefaultCache
-from data.source.nhlreference_source import NHLRefDataSource
 from api.webapi import webapi
 from api.admin import webapi_admin
 from db import init_db
 from db import db_session
+from config import configure_app
+from utils import get_instance_folder_path
 
 ALLOWED_EXTENSIONS = set(['csv'])
 
-app = Flask(__name__)
+app = Flask(__name__,
+            instance_path=get_instance_folder_path(),
+            instance_relative_config=True)
 
 def create_app(env):
-
-    root_logger = logging.getLogger()
-    root_logger.setLevel(logging.INFO)
-
-    fh = logging.FileHandler('/tmp/deepsportsanalytics.log')
-    fh.setLevel(logging.INFO)
-
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-
-    formatter = logging.Formatter('[%(asctime)s] [%(process)d:%(thread)d] \
-                                    [%(levelname)s] [%(module)s] [%(funcName)s] \
-                                    %(message)s')
-    ch.setFormatter(formatter)
-    fh.setFormatter(formatter)
-
-    root_logger.addHandler(ch)
-    root_logger.addHandler(fh)
-
-    if env == 'dev':
-        app.config.from_object('deepsportsanalytics.webapp.config.DevelopmentConfig')
-    if env == 'prod':
-        app.config.from_object('deepsportsanalytics.webapp.config.ProductionConfig')
-
-    app.logger.addHandler(ch)
-    app.logger.addHandler(fh)
-
+    configure_app(app, env)
+    # setup Blueprint
     app.register_blueprint(webapi)
     app.register_blueprint(webapi_admin)
-
     init_db()
 
     return app
--- deepsportsanalytics/webapp/config.py
+++ deepsportsanalytics/webapp/config.py
@@ -1,13 +1,48 @@
-class Config(object):
+import os
+import logging
+
+class BaseConfig(object):
     DEBUG = False
     DEVELOPMENT = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
     UPLOAD_FOLDER = '/tmp'
+    LOGGING_FORMAT = '[%(asctime)s] [%(process)d:%(thread)d] [%(levelname)s] [%(module)s] [%(funcName)s] %(message)s'
+    LOGGING_LOCATION = '/tmp/deepsportsanalytics.log'
+    LOGGING_LEVEL = logging.INFO
 
-class ProductionConfig(Config):
-    DEBUG = False
-
-class DevelopmentConfig(Config):
+class DevelopmentConfig(BaseConfig):
     DEVELOPMENT = True
     DEBUG = True
+    LOGGING_LEVEL = logging.DEBUG
+
+class TestConfig(BaseConfig):
+    DEVELOPMENT = False
+    DEBUG = False
+    LOGGING_LEVEL = logging.INFO
+
+__DEF_CONFIG = "deepsportsanalytics.webapp.config.DevelopmentConfig"
+__config = {
+    "development": "deepsportsanalytics.webapp.config.DevelopmentConfig",
+    "testing": "deepsportsanalytics.webapp.config.TestingConfig",
+    "default": __DEF_CONFIG
+}
+
+def configure_app(app, env):
+    app.config.from_object(__config.get(env, __DEF_CONFIG))
+    app.config.from_pyfile('config.ini', silent=True)
+
+    # setup logginig
+    root_logger = logging.getLogger()
+    root_logger.setLevel(app.config['LOGGING_LEVEL'])
+    fh = logging.FileHandler(app.config['LOGGING_LOCATION'])
+    fh.setLevel(app.config['LOGGING_LEVEL'])
+    ch = logging.StreamHandler()
+    ch.setLevel(app.config['LOGGING_LEVEL'])
+    formatter = logging.Formatter(app.config['LOGGING_FORMAT'])
+    ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+    root_logger.addHandler(ch)
+    root_logger.addHandler(fh)
+    app.logger.addHandler(ch)
+    app.logger.addHandler(fh)
